8e14f680ce9845bcff11ef482b0d154c19f68781
Shravan Amberkar <54074343+Shravan-Amberkar@users.noreply.github.com>
2019-09-26 01:14:10 +0530

    prints every 3 line

---
+++ print3line.py
@@ -0,0 +1,17 @@
+'''
+Write a Python program that reads input
+The input will consist of some number of lines of text. 
+The input will be terminated by a blank line.
+Your program should print every third line
+'''
+
+data=input()
+l=[]
+l.append(data)
+while data:
+    data=input()
+    if data=="":
+        break
+    l.append(data)
+for i in range(2,len(l),3):
+    print(l[i])
